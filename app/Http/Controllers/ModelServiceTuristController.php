1c6f5bcf2c2510962573c08de3f585d175ec5264
Corebitas <alex.soria@corebitsdev.com>
2018-11-25 11:01:42 -0500

    Base de datos Implementada sin revision

---
+++ app/Http/Controllers/ModelServiceTuristController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ModelServiceTurist;
+use Illuminate\Http\Request;
+
+class ModelServiceTuristController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ModelServiceTurist  $modelServiceTurist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ModelServiceTurist $modelServiceTurist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ModelServiceTurist  $modelServiceTurist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ModelServiceTurist $modelServiceTurist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ModelServiceTurist  $modelServiceTurist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ModelServiceTurist $modelServiceTurist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ModelServiceTurist  $modelServiceTurist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ModelServiceTurist $modelServiceTurist)
+    {
+        //
+    }
+}
---
+++ database/factories/ModelCreatePackageFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\ModelCreatePackage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
